0415a77d2a0bc4057bf3f69714d31ff704a0ca74
samolevich <samolevich@gmail.com>
2020-05-04 21:38:20 +0300

    create some components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import Header from './components/Header';
+import Navigation from './components/Navigation';
+import Content from './components/Content';
+import Footer from './components/Footer';
 
-function App() {
-  return <div>test</div>;
-}
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Navigation />
+      <Content />
+      <Footer />
+    </>
+  );
+};
 
 export default App;
