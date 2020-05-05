c4e2356f9014e0fc215fd143db06c16eb41831ca
samolevich <samolevich@gmail.com>
2020-05-05 22:57:17 +0300

    another color for active navbar link

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -5,18 +5,18 @@ import styles from './Navbar.module.css';
 class Navbar extends Component {
   render() {
     return (
-      <nav className={`${styles.list} ${styles.list.active}`}>
+      <nav className={styles.list}>
         <li>
           <NavLink to='/'>main</NavLink>
         </li>
         <li>
-          <NavLink to='/payload'>payload</NavLink>
+          <NavLink activeClassName={styles.current} to='/payload'>payload</NavLink>
         </li>
         <li>
-          <NavLink to='/post'>post</NavLink>
+          <NavLink activeClassName={styles.current} to='/post'>post</NavLink>
         </li>
         <li>
-          <NavLink to='/profile'>profile</NavLink>
+          <NavLink activeClassName={styles.current} to='/profile'>profile</NavLink>
         </li>
       </nav>
     );
