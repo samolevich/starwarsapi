f54364ade3078a29cd72cbfdd4ba85f74e3649bb
samolevich <samolevich@gmail.com>
2020-05-06 22:33:29 +0300

    read value from text area with ref

--- src/Components/Post/Post.jsx
+++ src/Components/Post/Post.jsx
@@ -2,15 +2,18 @@ import React, { Component } from 'react';
 import styles from './Post.module.css';
 
 class Post extends Component {
-  alerting() {
-    alert('Yo!');
-  };
+
+  postArea = React.createRef();
+
+  alerting = () => {
+    alert(this.postArea.current.value);
+  }
 
   render() {
     return (
       <div className={styles.post}>
         <h2>Posts</h2>
-        <textarea cols='40' rows='5'></textarea>
+        <textarea ref={this.postArea} cols='40' rows='5'></textarea>
         <br />
         <button onClick={this.alerting}>Add post</button>
       </div>
