584c8290e6584a305dbc1bce76a742e9bae12e60
samolevich <samolevich@gmail.com>
2020-05-06 21:24:36 +0300

    remove comments

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,15 +1,3 @@
-// const pullOfUsers = [
-//   { name: 'Rafael', route: 1 },
-//   { name: 'Michael', route: 2 },
-//   { name: 'Gabriel', route: 3 },
-//   { name: 'Asrael', route: 4 },
-//   { name: 'Asasel', route: 5 },
-//   { name: 'Sentiel', route: 6 },
-//   { name: 'Uriel', route: 7 },
-//   { name: 'Selaphiel', route: 8 },
-//   { name: 'Jegudiel', route: 9 },
-// ];
-
 const state = {
   pullOfUsers: [
     { name: 'Rafael', route: 1 },
