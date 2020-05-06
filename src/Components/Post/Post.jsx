a6d945b78951fdcddacefcdbded04f6a7d7d92b5
samolevich <samolevich@gmail.com>
2020-05-06 22:04:17 +0300

    create button event in Post Component

--- src/Components/Post/Post.jsx
+++ src/Components/Post/Post.jsx
@@ -1,8 +1,20 @@
 import React, { Component } from 'react';
+import styles from './Post.module.css';
 
 class Post extends Component {
+  alerting() {
+    alert('Yo!');
+  };
+
   render() {
-    return <div>Post</div>;
+    return (
+      <div className={styles.post}>
+        <h2>Posts</h2>
+        <textarea cols='40' rows='5'></textarea>
+        <br />
+        <button onClick={this.alerting}>Add post</button>
+      </div>
+    );
   }
 }
 
