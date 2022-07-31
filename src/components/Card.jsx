f08c5834409c013e836b2f192efb967261764810
samolevich <samolevich@gmail.com>
2022-07-31 18:55:40 +0300

    randomize Card color

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,45 +1,17 @@
 const Card = () => {
+  const randomCardColor = ["bg-success", "bg-danger", "bg-warning", "bg-info"];
+  const classNames =
+    "card text-white mb-3 " + randomCardColor[Math.floor(Math.random() * 4)];
+
   return (
-    <div>
-      <div className="card text-white bg-success mb-3">
-        <div className="card-header">Header</div>
-        <div className="card-body">
-          <h4 className="card-title">Success card title</h4>
-          <p className="card-text">
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </p>
-        </div>
-      </div>
-      <div className="card text-white bg-danger mb-3">
-        <div className="card-header">Header</div>
-        <div className="card-body">
-          <h4 className="card-title">Danger card title</h4>
-          <p className="card-text">
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </p>
-        </div>
-      </div>
-      <div className="card text-white bg-warning mb-3">
-        <div className="card-header">Header</div>
-        <div className="card-body">
-          <h4 className="card-title">Warning card title</h4>
-          <p className="card-text">
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </p>
-        </div>
-      </div>
-      <div className="card text-white bg-info mb-3">
-        <div className="card-header">Header</div>
-        <div className="card-body">
-          <h4 className="card-title">Info card title</h4>
-          <p className="card-text">
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </p>
-        </div>
+    <div className={classNames}>
+      <div className="card-header">Header</div>
+      <div className="card-body">
+        <h4 className="card-title">Success card title</h4>
+        <p className="card-text">
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </p>
       </div>
     </div>
   );
