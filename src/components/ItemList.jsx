214753990af7e7e46316a9e4628a63237b22a17a
samolevich <samolevich@gmail.com>
2022-08-08 00:14:25 +0300

    move event listener on parent/ change css link on remote source

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,14 +1,14 @@
 const ItemList = ({ list, onItemClick }) => {
   const itemList = list.map(({ id, name }) => (
-    <li
-      key={id}
-      className="list-group-item list-group-item-action"
-      onClick={() => onItemClick(id)}
-    >
+    <li key={id} className="list-group-item list-group-item-action" id={id}>
       {name}
     </li>
   ));
-  return <ul className="list-group">{itemList}</ul>;
+  return (
+    <ul className="list-group" onClick={event => onItemClick(event.target.id)}>
+      {itemList}
+    </ul>
+  );
 };
 
 export default ItemList;
